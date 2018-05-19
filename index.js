0d7d9ebd6f3dbef35ebc67efd5ff2bfc33294378
harshasanjeeva <harshasanjiva@gmail.com>
2018-05-19 08:07:53 -0700

    harsha

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+var url = "mongodb://harshasanjeeva:varsha1@ds229290.mlab.com:29290/295";
+
+// mongodb://<dbuser>:<dbpassword>@ds229290.mlab.com:29290/295
+const dbName = '295';
 var corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,
@@ -25,18 +31,38 @@ app.listen(3001, () => console.log('Server running on port 3001'))
 
 
 
-app.post('/api', (req, res) => {
-  
-  console.log(req.body)
-  console.log("query",req.body.data.query)
-  console.log("query",req.body.data.ll)
-  //req.body.data.ll
-  //var longe = 123;
-  var encry = cipher.encrypt('123')
-  console.log("cipher",encry)
-   var decry= cipher.decrypt(encry)
-  console.log("decry",decry)
+app.post('/api', (req, res) => {  
+  console.log("body of the incoming request",req.body)
+  console.log("User's requesting for:",req.body.data.query)
+  console.log("User's location:",req.body.data.ll)
+  var encry = cipher.encrypt('37')
+  var enlong = cipher.encrypt('122')
+  console.log("latitude encryption of the user",encry)
+  console.log("latitude encryption of the user",enlong) 
 
+
+  MongoClient.connect(url, function(err,db) {
+    if(err){
+      console.log("erjkbr",err)
+    }else{
+    console.log("successfull")
+   var col = db.collection('user');
+var data={
+  "lat":'37',
+  "long":'122',
+  "after_encryption_lat":encry,
+  "after_encryption_long":enlong
+}
+   // console.log("db",db)
+      col.insertOne({data},function(err, doc){
+        if(err){
+          console.log("err",err)
+        }else{
+          console.log("successfull")
+        }
+      })
+    }
+  });
 
 
   client.search(req.body.data.searchRequest).then(response => {
@@ -48,7 +74,6 @@ app.post('/api', (req, res) => {
 })
 
 app.post('/fake', (req, res) => {
-  
   console.log("1",)
   
 // var encry = cipher.encrypt(req.body.data)
@@ -58,6 +83,7 @@ app.post('/fake', (req, res) => {
 
   res.send({"message":"fake requests"})
 })
+
 
 
 
